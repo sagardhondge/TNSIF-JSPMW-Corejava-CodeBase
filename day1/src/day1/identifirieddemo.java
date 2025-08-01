696f9d6ff58091367959da02cc409bd14a3b834b
sagardhondge <sagardhondge56@gmail.com>
2025-08-01 12:48:52 +0530

    new commit

---
+++ day1/src/day1/identifirieddemo.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class identifirieddemo {
+
+	public static void main(String[] args) {
+		int $num =12;
+		System.out.println("value of the number varible is :"+ $num);
+		String studentName ="sagar";
+		System.out.println("value of the anme:"+ studentName);
+
+	}
+
+}

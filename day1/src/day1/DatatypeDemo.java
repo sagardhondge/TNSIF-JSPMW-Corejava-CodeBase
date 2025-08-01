2008f5f81382c65880b771134a84d4037eaeed4e
sagardhondge <sagardhondge56@gmail.com>
2025-08-01 13:19:40 +0530

    commit

---
+++ day1/src/day1/DatatypeDemo.java
@@ -0,0 +1,29 @@
+package day1;
+
+public class DatatypeDemo {
+
+	public static void main(String[] args) {
+		int value=9/2;
+		System.err.println(value);
+		float value1 =100f/3f;
+		System.err.println(value1);
+		double value2=110d/3d;
+		System.err.println(value2);
+		
+		int radius =2;
+		double percentage =3.14*radius*radius;
+		System.err.println(percentage);
+		
+		char ch='a';
+		System.err.println(ch);
+		
+		char ch1=65;
+		System.err.println(ch1);
+		if(ch==ch1) {
+			System.err.println("it is char");
+		}else System.err.println("it is not char");
+			
+		}
+
+}
+

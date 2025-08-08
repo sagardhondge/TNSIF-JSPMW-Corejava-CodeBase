5e58fa3ed23bbfca2fa04aff50294e25dd429769
sagardhondge <sagardhondge56@gmail.com>
2025-08-08 18:19:23 +0530

    demo classs using scanner

---
+++ day04/src/day04/persondemo.java
@@ -0,0 +1,48 @@
+package day04;
+
+import java.util.Scanner;
+
+public class persondemo {
+
+	public static void main(String[] args) {
+		
+		//scanner class
+		//scanner object to accept user inputs
+		Scanner ob=new Scanner(System.in);
+		
+		System.out.println("Enter Person Name");
+		String name=ob.next();
+		System.out.println("Enter Person Email");
+		String email=ob.next();
+		System.out.println("Enter age");
+		int age=ob.nextInt();
+		System.out.println("Enter gender");
+		String gender=ob.next();
+		System.out.println("Enter income");
+		int income=ob.nextInt();
+		
+		
+		//person class object and intialize vlaues
+		person person=new person();
+		person.setName(name);
+		person.setEmail(email);
+		person.setAge(age);
+		person.setGender(gender);
+		person.setIncome(income);
+
+//		calling an object one by one thorugh this method 	
+//		System.out.println(person.getAge());
+//		System.out.println(person.getGender());
+//		System.out.println(person);
+
+//		using to string we can call all the object at once
+		Taxcalculate calc=new Taxcalculate();
+		calc.calculateTax(person);
+		System.out.println("after calculation of tax");
+		
+		System.out.println(person);
+		
+		
+	}
+
+}

e3da4ec9d60e3543ba78069c246ba720c71b3262
sagardhondge <sagardhondge56@gmail.com>
2025-09-03 23:30:52 +0530

    constructordemo

--- day04/costructordemo.java
+++ day04/costructordemo.java
@@ -3,15 +3,48 @@ package day05;
 import java.util.Scanner;
 
 public class costructordemo {
-  public static void main(String[]args) {
-	  
-	  Scanner in = new Scanner(System.in);
-	  String name, city;
-	  int id;
-	  
-	  
-	  
-	  
-  }
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		String name, city;
+		int id;
+		System.out.println("Enter customer id :");
+		id=sc.nextInt();
+		
+		sc.nextLine();
+		System.out.println("Enter Customer name: ");
+		name=sc.nextLine();
+		
+		System.out.println("Enter Customer City :");
+		city=sc.nextLine();
+		
+		System.out.println(" ");
+		
+		
+		Customer c1=new Customer(); //default
+		c1.setCustomerCity(city);
+		c1.setCustomerId(id);
+		c1.setCustomername(name);
+		System.out.println(c1);
+		
+		System.out.println("Enter customer id :");
+		id=sc.nextInt();
+		
+		sc.nextLine();
+		System.out.println("Enter Customer name: ");
+		name=sc.nextLine();
+		
+		System.out.println("Enter Customer City :");
+		city=sc.nextLine();
+		
+		System.out.println(" ");
+		
+		Customer c2=new Customer(name, id, city);
+		System.out.println(c2);
+		
+		
+
+	}
+
 
 }

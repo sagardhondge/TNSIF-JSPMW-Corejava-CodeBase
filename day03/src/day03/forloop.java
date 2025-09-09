46147d62c8acd860d0cc9fa720f67f6ca9884409
sagardhondge <sagardhondge56@gmail.com>
2025-09-09 23:20:41 +0530

    Day03

--- day03/src/day03/forloop.java
+++ day03/src/day03/forloop.java
@@ -4,9 +4,9 @@ public class forloop {
 
 
 	public static void main(String[] args) {
-//		for(int i=0;i<=100;i++){
-//			System.out.print(i);
-//		}
+		for(int i=0;i<=100;i++){
+			System.out.print(i);
+		}
 
 	}
 

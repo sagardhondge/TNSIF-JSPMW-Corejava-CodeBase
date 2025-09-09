ddf3781648a72e9d6b3c9cff4d13879b6d55f6f5
sagardhondge <sagardhondge56@gmail.com>
2025-09-09 23:28:52 +0530

    Day01

---
+++ day01/src/day01/FirstProgram.java
@@ -0,0 +1,8 @@
+package day01;
+
+public class FirstProgram {
+	public static void main(String[] args) {
+
+        System.out.println("This is Sagar Dhondge");
+	}
+}
---
+++ day01/src/day01/Typecasting.java
@@ -0,0 +1,34 @@
+package day01;
+
+public class Typecasting {
+	public static void main(String[] args) {
+		//widing/narrowing  
+		byte b=10;
+		int l=b;
+		System.out.println(l);
+		
+		//float to double
+		float f=12.23f;
+		double d=f;
+		System.out.println(d);
+		
+		//
+		
+		// char to int
+		char ch = 'A';
+		int i=ch;
+		System.out.println(i);
+		
+		//narrowing
+		
+		//double to long
+		double fl =10.52f;
+		long ll = (long)fl;
+		System.out.println(ll);
+		
+		//int to byte
+		int i2 = 130;
+		byte bl=(byte)i2;
+		System.out.print(bl);
+	}
+}

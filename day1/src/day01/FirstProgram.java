eb00c5a0376e9a32452d679cc62f436b9418478b
sagardhondge <sagardhondge56@gmail.com>
2025-09-09 23:25:52 +0530

    day01

--- day1/src/day01/FirstProgram.java
+++
@@ -1,8 +0,0 @@
-package day01;
-
-public class FirstProgram {
-	public static void main(String[] args) {
-
-        System.out.println("This is Sagar Dhondge");
-	}
-}
--- day1/src/day01/loop.java
+++
@@ -1,9 +0,0 @@
-package day01;
-
-public class loop {
-	public static void main(String[] args) {
-		   for (int i = 1; i <= 10; i++) {
-	            System.out.println("Number: " + i);
-	}
-	}
-}

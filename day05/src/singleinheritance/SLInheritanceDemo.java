74c1b5e696073c77595c88496c8c7de07a33de3e
sagardhondge <sagardhondge56@gmail.com>
2025-09-10 10:33:15 +0530

    Inheritance

--- day05/src/singleinheritance/SLInheritanceDemo.java
+++ day05/src/singleinheritance/SLInheritanceDemo.java
@@ -4,7 +4,7 @@ public class SLInheritanceDemo {
 
 	public static void main(String[] args) {
 
-		Student s=new Student("Alina", "12345678", "Pune", 9876543210L, 1001, "JSPMW");
+		Student s=new Student("Sagar", "12345678", "Pune", 9876543210L, 1001, "JSPMW");
 		System.out.println(s);
 	}
 

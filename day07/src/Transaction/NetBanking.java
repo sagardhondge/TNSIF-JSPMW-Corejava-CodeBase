d7ae0f64ecf4e4a40bdd2f1a3efbf2216b5f757b
sagardhondge <sagardhondge56@gmail.com>
2025-09-17 23:15:46 +0530

    NetBanking Class

--- day07/src/Transaction/NetBanking.java
+++ day07/src/Transaction/NetBanking.java
@@ -1,5 +1,16 @@
 package Transaction;
 
-public class NetBanking {
+import java.util.Scanner;
 
+public class NetBanking extends MoneyWithdrawMethods {
+
+    @Override
+    public void processTransaction() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Net Banking User ID: ");
+        String userId = sc.nextLine();
+        System.out.print("Enter Password: ");
+        String password = sc.nextLine();
+        System.out.println("Transaction successful via Net Banking for user: " + userId);
+    }
 }

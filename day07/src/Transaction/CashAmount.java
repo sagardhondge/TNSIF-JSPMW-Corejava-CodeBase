dbec43a7269034706b7f1f963effed516e51687c
sagardhondge <sagardhondge56@gmail.com>
2025-09-17 23:10:53 +0530

    CashAmonut Class

--- day07/src/Transaction/CashAmount.java
+++ day07/src/Transaction/CashAmount.java
@@ -1,5 +1,11 @@
 package Transaction;
-
+import java.util.Scanner;
 public class CashAmount {
-
+    @Override
+    public void processTransaction() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Cash Amount: ");
+        double amount = sc.nextDouble();
+        System.out.println("Transaction successful. Cash Received: ₹" + amount);
+    }
 }

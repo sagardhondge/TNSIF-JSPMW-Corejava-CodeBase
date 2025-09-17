bbce4c197953f77c3f5636c2dc78982ee0ab23b2
sagardhondge <sagardhondge56@gmail.com>
2025-09-17 23:16:51 +0530

    Upi Class

--- day07/src/Transaction/UPI.java
+++ day07/src/Transaction/UPI.java
@@ -1,5 +1,16 @@
 package Transaction;
 
-public class UPI {
+import java.util.Scanner;
 
+public class UPI extends MoneyWithdrawMethods {
+
+    @Override
+    public void processTransaction() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter UPI ID: ");
+        String upiId = sc.nextLine();
+        System.out.print("Enter UPI PIN: ");
+        int upiPin = sc.nextInt();
+        System.out.println("Transaction successful via UPI with ID: " + upiId);
+    }
 }

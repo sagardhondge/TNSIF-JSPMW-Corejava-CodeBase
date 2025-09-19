7814ccee540ab194bff1508125289d9e223ae606
sagardhondge <sagardhondge56@gmail.com>
2025-09-19 21:43:30 +0530

    Day 08

---
+++ day08/src/day08/Shape.java
@@ -0,0 +1,10 @@
+package day08;
+
+public abstract class Shape {
+	protected float area;
+	public abstract void calarea();
+	
+	public void show(){
+		System.out.println("Area of shape :" + area);
+	}
+}
